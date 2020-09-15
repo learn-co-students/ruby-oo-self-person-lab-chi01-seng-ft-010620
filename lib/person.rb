0c0a31d3005b2c2bf7539e52efe94fc792f8c552
lindsayMecher <lindsaymecher@gmail.com>
2020-09-14 21:01:04 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,77 +1,67 @@
-# your code goes here
 class Person
-    attr_reader :name
-    attr_accessor :bank_account, :happiness, :hygiene
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
     def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
-        @name = name
+        @name = name 
         @bank_account = bank_account
         @happiness = happiness
         @hygiene = hygiene
-        
-        
-    end
-  
-    def happiness=(new_happiness)
-        if new_happiness > 10
-            @happiness = 10
-        elsif new_happiness < 0
-            @happiness = 0
-        else
-            @happiness = new_happiness
-        end
-        
+    end 
+
+    def happiness=(happiness)
+        @happiness = happiness.clamp(0,10)
+    end 
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene.clamp(0,10)
     end
 
-    def hygiene=(new_hygiene)
-        if new_hygiene > 10
-            @hygiene = 10
-        elsif new_hygiene < 0
-            @hygiene = 0
-        else
-            @hygiene = new_hygiene
-        end
-        
-    end
     def happy?
-        @happiness > 7
+        self.happiness > 7
     end
+
     def clean?
-        @hygiene > 7
+        self.hygiene > 7
     end
+
     def get_paid(salary)
-        @bank_account += salary
-        "all about the benjamins"
+        self.bank_account += salary
+        'all about the benjamins'
     end
+
     def take_bath
         self.hygiene += 4
-        "♪ Rub-a-dub just relaxing in the tub ♫"
-        
+        '♪ Rub-a-dub just relaxing in the tub ♫'
     end
+
     def work_out
-        self.hygiene -=3
+        self.hygiene -= 3
         self.happiness += 2
-        "♪ another one bites the dust ♫"
+        '♪ another one bites the dust ♫'
     end
+
     def call_friend(person)
         self.happiness += 3
         person.happiness += 3
         "Hi #{person.name}! It's #{self.name}. How are you?"
-
     end
-  
 
     def start_conversation(person, topic)
-        
-        if topic == 'politics'
-            self.happiness -= 2
+        if topic == "politics"
             person.happiness -= 2
-            'blah blah partisan blah lobbyist' 
-        elsif topic == 'weather'
-            self.happiness += 1
+            self.happiness -= 2
+            'blah blah partisan blah lobbyist'
+        elsif topic == "weather"
             person.happiness += 1
-            "blah blah sun blah rain"
-        else
-            "blah blah blah blah blah"
-        end 
+            self.happiness += 1
+            'blah blah sun blah rain'
+        else  
+            'blah blah blah blah blah'
+        end
+
     end
+
+
 end
